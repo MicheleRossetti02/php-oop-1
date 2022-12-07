aa06d5aa785c0aa383238d44bea0f053e72d5962
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-12-07 17:17:59 +0100

    film

--- index.php
+++ index.php
@@ -7,12 +7,14 @@ class Movie
     public $link;
     public $discount = 0;
     public $genere;
+    public $description;
 
-    public function __construct(string $title,  string $link, array $genere)
+    public function __construct(string $title,  string $link, array $genere, string $description)
     {
         $this->title = $title;
         $this->link = $link;
         $this->genere = $genere;
+        $this->description = $description;
     }
 
     public function setDiscount($age)
@@ -36,13 +38,15 @@ $generes = [
     new genere("Horror")
 ];
 
-$spiderman = new Movie("The Amazing Spiderman The Power Of Electro", "www.TheAmazingSpidermanThePowerOfElectro.com", $generes);
+$spiderman = new Movie("The Amazing Spiderman The Power Of Electro", "www.TheAmazingSpidermanThePowerOfElectro.com", $generes, ' Spider-Man combatte contro Rhino e il potente Electro mentre cerca di mantenere la promessa di non coinvolgere l\'amica Gwen nella sua vita pericolosa.');
 $spiderman->setDiscount(9);
 $discountspiderman = $spiderman->getDiscount();
 
-$matrix = new Movie("Matrix",  "www.matrix.com", $generes);
+
+$matrix = new Movie("Matrix",  "www.matrix.com", $generes, 'Matrix (The Matrix) è un film di fantascienza del 1999 in stile cyberpunk scritto e diretto dai fratelli Andy e Larry Wachowski.');
 $matrix->setDiscount(70);
 $discountmatrix = $matrix->getDiscount();
+
 // var_dump($discountspiderman);
 
 // var_dump($discountmatrix);
@@ -72,6 +76,11 @@ $discountmatrix = $matrix->getDiscount();
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer'>
     <link rel='stylesheet' href=''>
+    <style>
+        .row {
+            justify-content: space-between;
+        }
+    </style>
 </head>
 
 <body>
@@ -82,8 +91,8 @@ $discountmatrix = $matrix->getDiscount();
     <main id="site_main">
         <div class="container">
             <h1 class="text-center">CINEMA MOVIE</h1>
-            <div class="row">
-                <div class="col">
+            <div class="row ">
+                <div class="col-5">
                     <h3> <?= $spiderman->title ?> </h3>
                     <div class="info-film">
                         <div class="site d-flex">
@@ -98,7 +107,30 @@ $discountmatrix = $matrix->getDiscount();
                             <span class="ps-3"> <?= $spiderman->discount ?></span>
                         </div>
                     </div>
+                    <div class="description">
+                        <h3>DESCRIPTION:</h3>
+                        <p> <?= $spiderman->description ?></p>
+                    </div>
+                </div>
+                <div class="col-5">
+                    <h3> <?= $matrix->title ?> </h3>
+                    <div class="info-film">
+                        <div class="site d-flex">
+                            <h4>WEB SITE:</h4>
+                            <span class="ps-3">
+                                <?= $matrix->link ?>
+                            </span>
 
+                        </div>
+                        <div class="discount d-flex">
+                            <h3> DISCOUNT:</h3>
+                            <span class="ps-3"> <?= $matrix->discount ?></span>
+                        </div>
+                    </div>
+                    <div class="description">
+                        <h3>DESCRIPTION:</h3>
+                        <p> <?= $matrix->description ?></p>
+                    </div>
                 </div>
             </div>
         </div>
